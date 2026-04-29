d9b499bc8e99a3c689f6395276c0f6b9fb84dbb3
FabrizioRico <fabrizioriscoalva05@gmail.com>
2026-04-29 17:31:12 -0500

    [spring-course-difactura] commit inicial, estructura creada y clases model agregadas

---
+++ spring-course-difactura/src/main/java/com/fabrizio/spring/course/difactura/SpringCourseDifacturaApplication.java
@@ -0,0 +1,13 @@
+package com.fabrizio.spring.course.difactura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCourseDifacturaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCourseDifacturaApplication.class, args);
+	}
+
+}
---
+++ spring-course-difactura/src/test/java/com/fabrizio/spring/course/difactura/SpringCourseDifacturaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fabrizio.spring.course.difactura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCourseDifacturaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
