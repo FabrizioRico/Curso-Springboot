695f476fc28ef65542b71461ef0fac1d1cf6ff8c
FabrizioRico <fabrizioriscoalva05@gmail.com>
2026-05-07 16:31:15 -0500

    [spring-course-error] agregado exceptionHandler para 404

--- spring-course-error/src/main/java/com/fabrizio/spring/course/error/controller/HandlerExceptionController.java
+++ spring-course-error/src/main/java/com/fabrizio/spring/course/error/controller/HandlerExceptionController.java
@@ -6,6 +6,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.NoHandlerFoundException;
+
 import com.fabrizio.spring.course.error.model.Error;
 
 @RestControllerAdvice
@@ -18,9 +20,18 @@ public class HandlerExceptionController {
 		error.setError("Error de division por cero");
 		error.setMessage(ex.getMessage());
 		error.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
-		
-//		return ResponseEntity.internalServerError().body(error);
-		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR.value())
-				.body(error);
+//		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR.value()).body(error);
+		return ResponseEntity.internalServerError().body(error);
 	}
+	
+	@ExceptionHandler(NoHandlerFoundException.class)
+	public ResponseEntity<Error> notFoundEx(NoHandlerFoundException ex){
+		Error error = new Error();
+		error.setDate(new Date());
+		error.setError("Api Rest no encontrado");
+		error.setMessage(ex.getMessage());
+		error.setStatus(HttpStatus.NOT_FOUND.value());
+		return ResponseEntity.status(404).body(error);
+	}
+	
 }
