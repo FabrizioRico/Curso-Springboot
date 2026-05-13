817796885d7624a507a37fcd6e60fadecf885258
FabrizioRico <fabrizioriscoalva05@gmail.com>
2026-05-13 15:50:30 -0500

    [spring-course-interceptor] commit inicial, creado interceptor

---
+++ spring-course-interceptor/src/main/java/com/fabrizio/spring/course/app/interceptor/SpringCourseInterceptorApplication.java
@@ -0,0 +1,13 @@
+package com.fabrizio.spring.course.app.interceptor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCourseInterceptorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCourseInterceptorApplication.class, args);
+	}
+
+}
---
+++ spring-course-interceptor/src/test/java/com/fabrizio/spring/course/app/interceptor/SpringCourseInterceptorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fabrizio.spring.course.app.interceptor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCourseInterceptorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
